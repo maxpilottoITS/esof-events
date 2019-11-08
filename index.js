42162732ce19842b6bc5a918da1aad867872d82d
Max Pilotto <maxpil99@outlook.com>
2019-11-08 11:00:11 +0100

    Update example

--- index.js
+++ index.js
@@ -1,25 +1,56 @@
 const add = document.querySelector("#add")
-const del = document.querySelector("#delete")
-const list = document.querySelector("#expenseList")
-const total = document.querySelector("#total")
+const list = document.querySelector("#list")
+const rating = document.querySelector("#rating")
+const title = document.querySelector("#corso")
+const counter = document.querySelector("#counter")
+const corsi = document.querySelector("#corsi")
 
-const history = []
+let news = 0
 
-add.addEventListener('click', () => {
-    const title = document.querySelector("#title").value
-    const cost = document.querySelector("#import").value
-    const node = document.createElement("ion-item")
+updateCounter(0)
 
-    node.innerHTML = "<ion-label>" + title + " " + cost + "$</ion-label>"
-
-    history.push(new Expense(title,cost))
-    list.appendChild(node)
-    total.textContent = (parseInt(total.textContent) + parseInt(cost))
+corsi.addEventListener("click", () => {
+    updateCounter(news = 0)
 })
 
-class Expense{
-    constructor(title,cost){
-        this.title = title
-        this.cost = cost
+add.addEventListener('click', () => {
+    const itemNode = document.createElement("ion-item")
+    let ratingNode = ""
+
+    if (title.value == "") {
+        showToast("Il nome del corso non può essere vuoto!","danger")
+        return
     }
+
+    for (let i = 0; i < rating.value; i++) {
+        ratingNode += "<ion-icon name='heart'></ion-icon>"
+    }
+
+    itemNode.innerHTML += "<div>" + title.value + "</div>"
+    itemNode.innerHTML += "<div>" + ratingNode + "</div>"
+
+    list.appendChild(itemNode)
+
+    updateCounter(++news)
+})
+
+function updateCounter(count){
+    counter.innerHTML = count
+
+    if (count <= 0){
+        counter.style.display = "none"
+    }else{
+        counter.style.display = "block"
+    }
+}
+
+function showToast(message, color = "light") {
+    const toast = document.createElement('ion-toast')
+
+    toast.message = message
+    toast.duration = 2000
+    toast.color = color
+
+    document.body.appendChild(toast)
+    return toast.present()
 }
